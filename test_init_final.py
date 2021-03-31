30910a9bddad7eaf927acadd51ba0d997f1aed0a
scd0211 <72089101+scd0211@users.noreply.github.com>
2021-04-01 02:41:48 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1433,6 +1433,7 @@ class mainCog(commands.Cog):
 			command_list += ','.join(command[42]) + ' 사다리, 정산, 척살, 경주, 아이템\n'     #!채널삭제
 			command_list += ','.join(command[34]) + ' ※ 관리자만 실행 가능\n\n'     #서버나가기
 			command_list += ','.join(command[22]) + '\n'     #보스탐
+			command_list += ','.join(command[43]) + '\n'     #ㅂㅌ
 			command_list += ','.join(command[23]) + '\n'     #!보스탐
 			command_list += '[보스명]컷 또는 [보스명]컷 0000, 00:00\n'  
 			command_list += '[보스명] 컷 또는 [보스명] 컷 0000, 00:00\n'   
